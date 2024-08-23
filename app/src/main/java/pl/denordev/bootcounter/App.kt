956c0e03508f4df02f1c43fbee6142c5bdf597e7
Denis Orlov <denis.orlov@innowise.com>
2024-08-23 16:44:09 +0200

    Add todos

--- app/src/main/java/pl/denordev/bootcounter/App.kt
+++ app/src/main/java/pl/denordev/bootcounter/App.kt
@@ -5,3 +5,12 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application()
+
+
+/*TODO:
+    1. find out why Broadcast receiver doesn't work *_*
+    2. add worker for notifications
+    3. refactor Room to flow, to make code reactive
+    4. Add logic to ask user enable notifications
+    5. implement notifications handling according to the requirements ( cover all cases)
+*  */
